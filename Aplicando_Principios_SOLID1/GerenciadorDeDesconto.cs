1cdedccf3ad3fc3130810e4dee5384aea9934bdd
Alexya Moraes <alexyaviana@digix.com.br>
2022-09-28 17:14:23 -0400

    Resolvendo problemas de clean code (com classes a mais que eu modifiquei antes do git)

---
+++ Aplicando_Principios_SOLID1/GerenciadorDeDesconto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aplicando_Principios_SOLID1
+{
+    public class GerenciadorDeDesconto
+    {
+
+        public decimal AplicaDesconto(decimal precoProduto, StatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
+        {
+            decimal precoAposDesconto = 0;
+
+            decimal descontoPorFidelidade = (tempoDeContaEmAnos > Constantes.DESCONTO_MAXIMO_POR_FIDELIDADE) ?
+                                            (decimal)Constantes.DESCONTO_MAXIMO_POR_FIDELIDADE / 100 :
+                                            (decimal)tempoDeContaEmAnos / 100;
+
+            switch (statusContaCliente)
+            {
+                case StatusContaCliente.NaoRegistrado:
+                    precoAposDesconto = precoProduto;
+                    break;
+                case StatusContaCliente.ClienteComum:
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_COMUM * precoProduto));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                case StatusContaCliente.ClienteEspecial:
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_ESPECIAL * precoProduto));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                case StatusContaCliente.ClienteVIP:
+                    precoAposDesconto = (precoProduto - (Constantes.DESCONTO_CLIENTE_VIP * precoProduto));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            return precoAposDesconto;
+        }
+    }
+}
---
+++ Aplicando_Principios_SOLID1/StatusContaCliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Aplicando_Principios_SOLID1
+{
+    public enum StatusContaCliente
+    {
+        NaoRegistrado = 1,
+        ClienteComum = 2,
+        ClienteEspecial = 3,
+        ClienteVIP = 4
+    }
+
+}
